87d2985e4b68dc0760e3afbd6c5c2cd79c5c08af
Brandon Satrom <brsatrom@microsoft.com>
2011-11-21 17:44:23 -0600

    Added functionality to call text endpoint when Generate button is clicked.

--- public/javascripts/index.coffee
+++ public/javascripts/index.coffee
@@ -3,4 +3,11 @@
 		$(@).addClass('clicked').delay(300).queue (next) ->
 			$(@).removeClass 'clicked'
 			next()		
+	$('#generate').click ->
+		multiplier = $('#exterminateMultiplier option:selected').text()
+		paragraphs = $('#numParagraphs option:selected').text()
+		
+		$.ajaxSetup { cache: false }
+		$.getJSON "/text/#{multiplier}/#{paragraphs}", (data) ->
+			$('#ipsumText').val(data.text)
 )(jQuery)
--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,9 +1,20 @@
 (function() {
   (function($) {
-    return $('a').click(function() {
+    $('a').click(function() {
       return $(this).addClass('clicked').delay(300).queue(function(next) {
         $(this).removeClass('clicked');
         return next();
+      });
+    });
+    return $('#generate').click(function() {
+      var multiplier, paragraphs;
+      multiplier = $('#exterminateMultiplier option:selected').text();
+      paragraphs = $('#numParagraphs option:selected').text();
+      $.ajaxSetup({
+        cache: false
+      });
+      return $.getJSON("/text/" + multiplier + "/" + paragraphs, function(data) {
+        return $('#ipsumText').val(data.text);
       });
     });
   })(jQuery);
